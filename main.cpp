fe1e38b1a393da6799fcfb65417b1abb8eecaefd
KoroList <zeicauliaisz@gmail.com>
2023-05-22 15:23:44 +0200

    Fixed segfault

--- main.cpp
+++ main.cpp
@@ -14,6 +14,7 @@
 SDL_Surface* scr;
 
 
+
 class ColorRGB8bit {
     public:
         Uint8 r;
@@ -124,6 +125,8 @@ int main(int /*argc*/, char */*argv*/[])
   double posX = 22, posY = 12;  //x and y start position
   double dirX = -1, dirY = 0; //initial direction vector
   double planeX = 0, planeY = 0.66; //the 2d raycaster version of camera plane
+
+  scr = SDL_CreateRGBSurface(0, screenWidth, screenHeight, 32, 0, 0, 0, 0);
 
   double time = 0; //time of current frame
   double oldTime = 0; //time of previous frame 
